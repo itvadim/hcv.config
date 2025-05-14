891152278f9a51f5d1b9564049c1e2f5ca388f70
Vadim Titkov <tvadim@t100.group>
2025-05-15 00:19:29 +0300

    Add AppRole auth support for Vault configuration
    - Introduced `UseAppRoleAuth` extension method for configuring AppRole authentication with HashiCorp Vault.

---
+++ src/T100.Extensions.Configuration.HashiCorpVault.Secrets/Extensions/VaultSecretAppRoleAuthConfigurationExtensions.cs
@@ -0,0 +1,56 @@
+﻿namespace T100.Extensions.Configuration.HashiCorpVault.Secrets.Extensions;
+
+using System;
+using AuthProviders;
+using Microsoft.Extensions.Configuration;
+
+/// <summary>
+/// Provides extension methods for configuring HashiCorp Vault integration
+/// using the AppRole authentication method.
+/// </summary>
+public static class VaultSecretAppRoleAuthConfigurationExtensions
+{
+	/// <summary>
+	/// Configures the application to use AppRole authentication with HashiCorp Vault.
+	/// </summary>
+	/// <param name="builder">
+	/// The configuration builder that provides the context for HashiCorp Vault integration.
+	/// </param>
+	/// <param name="roleId">
+	/// The Role ID used for AppRole authentication.
+	/// </param>
+	/// <param name="secretId">
+	/// The Secret ID used for AppRole authentication.
+	/// </param>
+	/// <returns>
+	/// The <see cref="IConfigurationBuilder"/> with AppRole authentication configured.
+	/// </returns>
+	/// <exception cref="InvalidOperationException">
+	/// Thrown if the provided builder is not a valid <see cref="VaultSecretConfigurationBuilder"/>.
+	/// </exception>
+	public static IConfigurationBuilder UseAppRoleAuth
+	(
+		this IVaultSecretConfigurationBuilder builder,
+		string roleId,
+		string secretId
+	)
+	{
+		if (builder is not VaultSecretConfigurationBuilder configuration)
+		{
+			throw new InvalidOperationException();
+		}
+
+		return builder.Add
+		(
+			new VaultSecretConfigurationSource
+			(
+				configuration.VaultHost, 
+				configuration.MountPoint, 
+				configuration.Prefix, 
+				configuration.SecretPath, 
+				new AppRoleVaultAuthProvider(roleId, secretId), 
+				configuration.Optional
+			)
+		);
+	}
+}
---
+++ test/T100.Extensions.Configuration.HashiCorpVault.Secrets.UnitTests/VaultSecretAppRoleAuthConfigurationExtensionsShould.cs
@@ -0,0 +1,73 @@
+﻿namespace T100.Extensions.Configuration.HashiCorpVault.Secrets.UnitTests;
+
+using AuthProviders;
+using Extensions;
+using Microsoft.Extensions.Configuration;
+using Moq;
+
+public class VaultSecretAppRoleAuthConfigurationExtensionsShould
+{
+	private const string VaultHost = "https://vault-server";
+	private const string MountPoint = "secrets";
+	private const string Prefix = "my-app:";
+	private const string SecretPath1 = "path1";
+
+	[Fact]
+	public void ThrowInvalidOperationExceptionWhenBuilderTypeIsInvalid()
+	{
+		// Arrange
+		var invalidBuilderMock = new Mock<IVaultSecretConfigurationBuilder>();
+		const string roleId = "role-id";
+		const string secretId = "secret-id";
+
+		// Act
+		Action act = () => invalidBuilderMock.Object.UseAppRoleAuth(roleId, secretId);
+
+		// Assert
+		act.Should().Throw<InvalidOperationException>();
+	}
+
+	[Fact]
+	public void AddVaultSecretConfigurationSourceSuccessfully()
+	{
+		// Arrange
+		var configurationBuilderMock = new Mock<IConfigurationBuilder>();
+		configurationBuilderMock
+			.Setup(x => x.Add(It.IsAny<VaultSecretConfigurationSource>()))
+			.Returns(configurationBuilderMock.Object);
+
+		var builder = new VaultSecretConfigurationBuilder
+		(
+			configurationBuilderMock.Object,
+			VaultHost,
+			MountPoint,
+			Prefix,
+			[SecretPath1]
+		);
+        
+		const string roleId = "valid-role-id";
+		const string secretId = "valid-secret-id";
+
+		// Act
+		var result = builder.UseAppRoleAuth(roleId, secretId);
+
+		// Assert
+		result.Should().Be(configurationBuilderMock.Object);
+
+		configurationBuilderMock.Verify
+		(
+			x => x.Add
+			(
+				It.Is<VaultSecretConfigurationSource>
+				(
+					s => 
+					s.VaultHost == VaultHost &&
+					s.MountPoint == MountPoint &&
+					s.Prefix == Prefix &&
+					s.SecretPath.SequenceEqual(new[] { SecretPath1 }) &&
+					s.AuthProvider.GetType() == typeof(AppRoleVaultAuthProvider))
+			),
+			Times.Once
+		);
+	}
+}
